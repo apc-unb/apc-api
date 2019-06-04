a49c1bb6b1d6b974a3dfeada8458e2d181577529
Thiago Veras <thiago.veras.machado@gmail.com>
2019-06-04 00:01:16 -0300

    Starting Creating User (80% done, need to get json from front end )

--- server/routers.go
+++ server/routers.go
@@ -1,10 +1,49 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"plataforma-apc/components/student"
 )
 
 func (a *App) createUsers(w http.ResponseWriter, r *http.Request) {
+
+	studentClass1 := student.Student{
+		FirstName: "Thiago",
+		LastName:  "Veras Machado",
+		Matricula: "160156666",
+		Handles:   []string{"Veras", "113065"},
+		Password:  "HQFnf-1234",
+		PhotoUrl:  "https://userpic.codeforces.com/546204/title/d2ac05baf39339f.jpg",
+		Grade:     8.98,
+	}
+
+	studentClass2 := student.Student{
+		FirstName: "Vitor",
+		LastName:  "Fernandes Dullens",
+		Matricula: "160571946",
+		Handles:   []string{"vitordullens", "2353251"},
+		Password:  "Hgqwge1234",
+		PhotoUrl:  "https://userpic.codeforces.com/551311/title/95d04d8b95b95302.jpg",
+		Grade:     9.08,
+	}
+
+	studentClass3 := student.Student{
+		FirstName: "Giovanni",
+		LastName:  "Guidini",
+		Matricula: "136246666",
+		Handles:   []string{"Gguidini", "11165"},
+		Password:  "12rw-1234",
+		PhotoUrl:  "https://userpic.codeforces.com/765049/title/2075d6432eadaae9.jpg",
+		Grade:     9.98,
+	}
+
+	defer a.DB.Disconnect(context.TODO())
+
+	if err := student.CreateStudent(a.DB, []student.Student{studentClass1, studentClass2, studentClass3}); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
 
 	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "success"})
 }
