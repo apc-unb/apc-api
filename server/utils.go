adeb9fda3f7066fc6f6756cbcc24434f4e9a8742
Thiago Veras <thiago.veras.machado@gmail.com>
2019-06-07 00:04:41 -0300

    Provisionally copied GetMondoDB function to utils (need to fix that)

--- server/utils.go
+++ server/utils.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"log"
 	"net/http"
+
+	"github.com/mongodb/mongo-go-driver/mongo"
 )
 
 func respondWithError(w http.ResponseWriter, code int, message string) {
@@ -15,4 +19,21 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(response)
+}
+
+func GetMongoDB(host, port string) (*mongo.Client, error) {
+
+	db, err := mongo.Connect(context.TODO(), "mongodb://"+host+":"+port)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping(context.TODO(), nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
