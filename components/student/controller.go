b8fccf23a50df91a483cc910b896d2c30a996606
Thiago Veras <thiago.veras.machado@gmail.com>
2019-06-10 22:46:03 -0300

    Implemented all functions to pass ALL db tests (100% working)

--- components/student/controller.go
+++ components/student/controller.go
@@ -7,6 +7,16 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
+// TODO : Insert all students at the same time (if possible)
+// Recieve a list of students
+// Checks if that list is not null (can't insert null list)
+// Insert each student individually in database
+// @param	db				pointer to database
+// @param	students 		list of students
+// @param	database_name	name of database
+// @param	collection_name	name of collection
+// @return 	error 			function error
+
 func CreateStudent(db *mongo.Client, students []Student, database_name, collection_name string) error {
 
 	if len(students) == 0 {
@@ -16,14 +26,22 @@ func CreateStudent(db *mongo.Client, students []Student, database_name, collecti
 	collection := db.Database(database_name).Collection(collection_name)
 
 	for _, student := range students {
-		_, err := collection.InsertOne(context.TODO(), student)
-		if err != nil {
+		if _, err := collection.InsertOne(context.TODO(), student); err != nil {
 			return err
 		}
 	}
 
 	return nil
 }
+
+// Return list of all students from Database
+// Get all students at the same time and store inside cursor
+// Decode each student inside student class and append into students array
+// @param	db				pointer to database
+// @param	database_name	name of database
+// @param	collection_name	name of collection
+// @return 	[]Student		list of all students
+// @return 	error 			function error
 
 func GetStudents(db *mongo.Client, database_name, collection_name string) ([]Student, error) {
 
@@ -33,20 +51,20 @@ func GetStudents(db *mongo.Client, database_name, collection_name string) ([]Stu
 	students := []Student{}
 
 	// Passing bson.D{{}} as the filter matches all documents in the collection
-	cur, err := collection.Find(context.TODO(), bson.D{{}}, nil)
+	cursor, err := collection.Find(context.TODO(), bson.D{{}}, nil)
 	if err != nil {
 		return nil, err
 	}
 
 	// Finding multiple documents returns a cursor
 	// Iterating through the cursor allows us to decode documents one at a time
-	for cur.Next(context.TODO()) {
+	for cursor.Next(context.TODO()) {
 
 		// create a value into which the single document can be decoded
 		var elem Student
 
 		// Checks if decoding method didn't return any errors
-		if err := cur.Decode(&elem); err != nil {
+		if err := cursor.Decode(&elem); err != nil {
 			return nil, err
 		}
 
@@ -54,12 +72,70 @@ func GetStudents(db *mongo.Client, database_name, collection_name string) ([]Stu
 		students = append(students, elem)
 	}
 
-	if err := cur.Err(); err != nil {
+	if err := cursor.Err(); err != nil {
 		return nil, err
 	}
 
 	// Close the cursor once finished
-	cur.Close(context.TODO())
+	cursor.Close(context.TODO())
 
 	return students, nil
+}
+
+// TODO : Update all students at the same time (if possible)
+// Recieve a list of students (updated)
+// Checks if that list is not null (can't update null list)
+// Update each student individually
+// @param	db				pointer to database (updated)
+// @param	students 		list of students
+// @param	database_name	name of database
+// @param	collection_name	name of collection
+// @return 	[]Student		list of all students
+// @return 	error 			function error
+
+func UpdateStudent(db *mongo.Client, students []Student, database_name, collection_name string) error {
+
+	if len(students) == 0 {
+		return nil
+	}
+
+	collection := db.Database(database_name).Collection(collection_name)
+
+	for _, student := range students {
+		filter := bson.M{"_id": student.ID}
+		update := bson.M{"$set": student}
+		if _, err := collection.UpdateOne(context.TODO(), filter, update, nil); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// TODO : Delete all students at the same time (if possible)
+// Recieve a list of students (to be deleted)
+// Checks if that list is not null (can't delete null list)
+// Delete each student individually
+// @param	db				pointer to database (to be deleted)
+// @param	students 		list of students
+// @param	database_name	name of database
+// @param	collection_name	name of collection
+// @return 	[]Student		list of all students
+// @return 	error 			function error
+
+func DeleteStudent(db *mongo.Client, students []Student, database_name, collection_name string) error {
+
+	if len(students) == 0 {
+		return nil
+	}
+
+	collection := db.Database(database_name).Collection(collection_name)
+
+	for _, student := range students {
+		filter := bson.M{"_id": student.ID}
+		if _, err := collection.DeleteOne(context.TODO(), filter); err != nil {
+			return err
+		}
+	}
+	return nil
+
 }
