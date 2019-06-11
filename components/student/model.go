4bb65e622f2212bcba3fd102cb8099c371148581
Thiago Veras <thiago.veras.machado@gmail.com>
2019-06-10 22:45:26 -0300

    Changing ID (int) to (bson.ObjectId) in order to get mongo db ID

--- components/student/model.go
+++ components/student/model.go
@@ -1,12 +1,14 @@
 package student
 
+import "gopkg.in/mgo.v2/bson"
+
 type Student struct {
-	ID        int      `json:"id"`
-	FirstName string   `json:"firstname"`
-	LastName  string   `json:"lastname"`
-	Matricula string   `json:"matricula"`
-	Handles   []string `json:"handles"`
-	Password  string   `json:"password"`
-	PhotoUrl  string   `json:"photourl"`
-	Grade     float64  `json:"grade"`
+	ID        bson.ObjectId `json:"id" bson:"_id"`
+	FirstName string        `json:"firstname"`
+	LastName  string        `json:"lastname"`
+	Matricula string        `json:"matricula"`
+	Handles   []string      `json:"handles"`
+	Password  string        `json:"password"`
+	PhotoUrl  string        `json:"photourl"`
+	Grade     float64       `json:"grade"`
 }
