0810a1cbd42b290c36659b6340c90bed14305603
verasthiago <thiago.veras.machado@gmail.com>
2019-07-04 18:22:53 -0300

    created the rest of controllers (working but need comments)

--- components/contest/controller.go
+++ components/contest/controller.go
@@ -1,35 +1,35 @@
-package task
+package contest
 
 import (
 	"context"
-	"go.mongodb.org/mongo-driver/bson"
 	"github.com/mongodb/mongo-go-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
-func CreateTasks(db *mongo.Client, tasks []Task, database_name, collection_name string) error {
+func CreateContests(db *mongo.Client, contests []Contest, database_name, collection_name string) error {
 	
-	if len(tasks) == 0 {
+	if len(contests) == 0 {
 		return nil
 	}
 
 	collection := db.Database(database_name).Collection(collection_name)
 
-	for _, task := range tasks {
-		if _, err := collection.InsertOne(context.TODO(), task); err != nil {
+	for _, contest := range contests {
+		if _, err := collection.InsertOne(context.TODO(), contest); err != nil {
 			return err
 		}
 	}
 
 	return nil
-
+	
 }
 
-func GetTasks(db *mongo.Client, database_name, collection_name string) ([]Task, error) {
+func GetContests(db *mongo.Client, database_name, collection_name string) ([]Contest, error) {
 	
 	collection := db.Database(database_name).Collection(collection_name)
 
 	// Here's an array in which you can store the decoded documents
-	tasks := []Task{}
+	contests := []Contest{}
 
 	// Passing bson.D{{}} as the filter matches all documents in the collection
 	cursor, err := collection.Find(context.TODO(), bson.D{{}}, nil)
@@ -42,7 +42,7 @@ func GetTasks(db *mongo.Client, database_name, collection_name string) ([]Task, 
 	for cursor.Next(context.TODO()) {
 
 		// create a value into which the single document can be decoded
-		var elem Task
+		var elem Contest
 
 		// Checks if decoding method didn't return any errors
 		if err := cursor.Decode(&elem); err != nil {
@@ -50,7 +50,7 @@ func GetTasks(db *mongo.Client, database_name, collection_name string) ([]Task, 
 		}
 
 		// Push school class inside student array
-		tasks = append(tasks, elem)
+		contests = append(contests, elem)
 	}
 
 	if err := cursor.Err(); err != nil {
@@ -60,21 +60,21 @@ func GetTasks(db *mongo.Client, database_name, collection_name string) ([]Task, 
 	// Close the cursor once finished
 	cursor.Close(context.TODO())
 
-	return tasks, nil
+	return contests, nil
 
 }
 
-func UpdateTasks(db *mongo.Client, tasks []Task, database_name, collection_name string) error {
+func UpdateContests(db *mongo.Client, contests []Contest, database_name, collection_name string) error {
 	
-	if len(tasks) == 0 {
+	if len(contests) == 0 {
 		return nil
 	}
 
 	collection := db.Database(database_name).Collection(collection_name)
 
-	for _, task := range tasks {
-		filter := bson.M{"_id": task.ID}
-		update := bson.M{"$set": task}
+	for _, contest := range contests {
+		filter := bson.M{"_id": contest.ID}
+		update := bson.M{"$set": contest}
 		if _, err := collection.UpdateOne(context.TODO(), filter, update, nil); err != nil {
 			return err
 		}
@@ -83,20 +83,20 @@ func UpdateTasks(db *mongo.Client, tasks []Task, database_name, collection_name 
 
 }
 
-func DeleteTasks(db *mongo.Client, tasks []Task, database_name, collection_name string) error {
+func DeleteContests(db *mongo.Client, contests []Contest, database_name, collection_name string) error {
 	
-	if len(tasks) == 0 {
+	if len(contests) == 0 {
 		return nil
 	}
 
 	collection := db.Database(database_name).Collection(collection_name)
 
-	for _, task := range tasks {
-		filter := bson.M{"_id": task.ID}
+	for _, contest := range contests {
+		filter := bson.M{"_id": contest.ID}
 		if _, err := collection.DeleteOne(context.TODO(), filter); err != nil {
 			return err
 		}
 	}
 	return nil
-
+	
 }
--- components/contest/model.go
+++ components/contest/model.go
@@ -3,11 +3,13 @@ package contest
 import (
 	"plataforma-apc/components/schoolClass"
 	"plataforma-apc/components/task"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type Contest struct {
-	ID    int                     `json:"id"`
+	ID    bson.ObjectId     	  `json:"id" bson:"_id"`
 	Date  string                  `json:"date"`
 	Class schoolClass.SchoolClass `json:"class"`
 	Tasks []task.Task             `json:"tasks"`
 }
+
