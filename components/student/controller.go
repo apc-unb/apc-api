ec9eac542450e23137d8ae1207f7059869312412
Thiago Veras <thiago.veras.machado@gmail.com>
2019-07-14 00:40:30 -0300

    adding AuthStudent method (temporary)

--- components/student/controller.go
+++ components/student/controller.go
@@ -7,23 +7,22 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-// TODO : Insert all students at the same time (if possible)
-// Recieve a list of students
+// CreateStudents recieve a list of students
 // Checks if that list is not null (can't insert null list)
 // Insert each student individually in database
 // @param	db				pointer to database
 // @param	students 		list of students
-// @param	database_name	name of database
-// @param	collection_name	name of collection
+// @param	databaseName	name of database
+// @param	collectionName	name of collection
 // @return 	error 			function error
-
-func CreateStudents(db *mongo.Client, students []Student, database_name, collection_name string) error {
+// TODO : Insert all students at the same time (if possible)
+func CreateStudents(db *mongo.Client, students []StudentCreate, databaseName, collectionName string) error {
 
 	if len(students) == 0 {
 		return nil
 	}
 
-	collection := db.Database(database_name).Collection(collection_name)
+	collection := db.Database(databaseName).Collection(collectionName)
 
 	for _, student := range students {
 		if _, err := collection.InsertOne(context.TODO(), student); err != nil {
@@ -34,24 +33,24 @@ func CreateStudents(db *mongo.Client, students []Student, database_name, collect
 	return nil
 }
 
-// Return list of all students from Database
+// GetStudents return list of all students from Database
 // Get all students at the same time and store inside cursor
 // Decode each student inside student class and append into students array
 // @param	db				pointer to database
-// @param	database_name	name of database
-// @param	collection_name	name of collection
+// @param	databaseName	name of database
+// @param	collectionName	name of collection
 // @return 	[]Student		list of all students
 // @return 	error 			function error
+func GetStudents(db *mongo.Client, databaseName, collectionName string) ([]Student, error) {
 
-func GetStudents(db *mongo.Client, database_name, collection_name string) ([]Student, error) {
-
-	collection := db.Database(database_name).Collection(collection_name)
+	collection := db.Database(databaseName).Collection(collectionName)
 
 	// Here's an array in which you can store the decoded documents
 	students := []Student{}
 
 	// Passing bson.D{{}} as the filter matches all documents in the collection
 	cursor, err := collection.Find(context.TODO(), bson.D{{}}, nil)
+
 	if err != nil {
 		return nil, err
 	}
@@ -68,6 +67,8 @@ func GetStudents(db *mongo.Client, database_name, collection_name string) ([]Stu
 			return nil, err
 		}
 
+		elem.Password = ""
+
 		// Push student inside student array
 		students = append(students, elem)
 	}
@@ -82,24 +83,23 @@ func GetStudents(db *mongo.Client, database_name, collection_name string) ([]Stu
 	return students, nil
 }
 
-// TODO : Update all students at the same time (if possible)
-// Recieve a list of students (updated)
+// UpdateStudents recieve a list of students (updated)
 // Checks if that list is not null (can't update null list)
 // Update each student individually
 // @param	db				pointer to database (updated)
 // @param	students 		list of students
-// @param	database_name	name of database
-// @param	collection_name	name of collection
+// @param	databaseName	name of database
+// @param	collectionName	name of collection
 // @return 	[]Student		list of all students
 // @return 	error 			function error
-
-func UpdateStudents(db *mongo.Client, students []Student, database_name, collection_name string) error {
+// TODO : Update all students at the same time (if possible)
+func UpdateStudents(db *mongo.Client, students []StudentUpdate, databaseName, collectionName string) error {
 
 	if len(students) == 0 {
 		return nil
 	}
 
-	collection := db.Database(database_name).Collection(collection_name)
+	collection := db.Database(databaseName).Collection(collectionName)
 
 	for _, student := range students {
 		filter := bson.M{"_id": student.ID}
@@ -111,24 +111,23 @@ func UpdateStudents(db *mongo.Client, students []Student, database_name, collect
 	return nil
 }
 
-// TODO : Delete all students at the same time (if possible)
-// Recieve a list of students (to be deleted)
+// DeleteStudents recieve a list of students (to be deleted)
 // Checks if that list is not null (can't delete null list)
 // Delete each student individually
 // @param	db				pointer to database (to be deleted)
 // @param	students 		list of students
-// @param	database_name	name of database
-// @param	collection_name	name of collection
+// @param	databaseName	name of database
+// @param	collectionName	name of collection
 // @return 	[]Student		list of all students
 // @return 	error 			function error
-
-func DeleteStudents(db *mongo.Client, students []Student, database_name, collection_name string) error {
+// TODO : Delete all students at the same time (if possible)
+func DeleteStudents(db *mongo.Client, students []Student, databaseName, collectionName string) error {
 
 	if len(students) == 0 {
 		return nil
 	}
 
-	collection := db.Database(database_name).Collection(collection_name)
+	collection := db.Database(databaseName).Collection(collectionName)
 
 	for _, student := range students {
 		filter := bson.M{"_id": student.ID}
@@ -138,4 +137,28 @@ func DeleteStudents(db *mongo.Client, students []Student, database_name, collect
 	}
 	return nil
 
+}
+
+// AuthStudent recieve a student (to be authenticated)
+// Checks if that date exist in databse
+// Return true if exist
+// @param	db				pointer to database (to be deleted)
+// @param	student			student matricula and password
+// @param	databaseName	name of database
+// @param	collectionName	name of collection
+// @return 	[]bool			user exist veredict
+// @return 	error 			function error
+func AuthStudent(db *mongo.Client, student StudentLogin, databaseName, collectionName string) (Student, error) {
+
+	collection := db.Database(databaseName).Collection(collectionName)
+
+	findStudent := Student{}
+
+	filter := bson.D{{"matricula", student.Matricula}, {"password", student.Password}}
+
+	if err := collection.FindOne(context.TODO(), filter).Decode(&findStudent); err != nil {
+		return findStudent, err
+	}
+
+	return findStudent, nil
 }
