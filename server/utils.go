99db7457cf1c4d2336507a2cd0dfcc9ddf96933d
Thiago Veras <thiago.veras.machado@gmail.com>
2019-07-14 00:36:51 -0300

    removing get mongo con (need to fix this)

--- server/utils.go
+++ server/utils.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
-	"log"
 	"net/http"
-
-	"github.com/mongodb/mongo-go-driver/mongo"
 )
 
 func respondWithError(w http.ResponseWriter, code int, message string) {
@@ -19,21 +15,4 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(response)
-}
-
-func GetMongoDB(host, port string) (*mongo.Client, error) {
-
-	db, err := mongo.Connect(context.TODO(), "mongodb://"+host+":"+port)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = db.Ping(context.TODO(), nil)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
 }
