cd956119e74aa04849295c95922b1709f3785cd0
Thiago Veras <thiago.veras.machado@gmail.com>
2019-07-17 15:19:54 -0300

    implementing the functions missing

--- server/routers.go
+++ server/routers.go
@@ -1,42 +1,23 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
 
+	"github.com/plataforma-apc/components/contest"
 	"github.com/plataforma-apc/components/news"
 	"github.com/plataforma-apc/components/schoolClass"
 	"github.com/plataforma-apc/components/student"
+	"github.com/plataforma-apc/components/submission"
+	"github.com/plataforma-apc/components/task"
 )
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 // 									 STUDENTS			 								 //
 ///////////////////////////////////////////////////////////////////////////////////////////
 
-func (a *App) createStudents(w http.ResponseWriter, r *http.Request) {
-
-	var students []student.StudentCreate
-
-	decoder := json.NewDecoder(r.Body)
-
-	if err := decoder.Decode(&students); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-		return
-	}
-
-	defer r.Body.Close()
-
-	if err := student.CreateStudents(a.DB, students, "apc_database", "student"); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "success"})
-}
-
-func (a *App) getStudent(w http.ResponseWriter, r *http.Request) {
+func (a *App) getStudentLogin(w http.ResponseWriter, r *http.Request) {
 
 	var studentLogin student.StudentLogin
 	var singleStudent student.StudentInfo
@@ -76,6 +57,27 @@ func (a *App) getStudent(w http.ResponseWriter, r *http.Request) {
 	}
 
 	respondWithJSON(w, http.StatusOK, ret)
+}
+
+func (a *App) createStudents(w http.ResponseWriter, r *http.Request) {
+
+	var students []student.StudentCreate
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&students); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := student.CreateStudents(a.DB, students, "apc_database", "student"); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "success"})
 }
 
 func (a *App) getStudents(w http.ResponseWriter, r *http.Request) {
@@ -136,11 +138,19 @@ func (a *App) deleteStudents(w http.ResponseWriter, r *http.Request) {
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 func (a *App) createClasses(w http.ResponseWriter, r *http.Request) {
-	// Temporary
-	collection := a.DB.Database("apc_database").Collection("schoolClass")
-	collection.Drop(context.TODO())
 
-	if err := schoolClass.CreateClasses(a.DB, []schoolClass.SchoolClassCreate{}, "apc_database", "schoolClass"); err != nil {
+	var classes []schoolClass.SchoolClassCreate
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&classes); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := schoolClass.CreateClasses(a.DB, classes, "apc_database", "schoolClass"); err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
@@ -149,15 +159,58 @@ func (a *App) createClasses(w http.ResponseWriter, r *http.Request) {
 }
 
 func (a *App) getClasses(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	classes, err := schoolClass.GetClasses(a.DB, "apc_database", "schoolClass")
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, classes)
+
 }
 
 func (a *App) updateClasses(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	var classes []schoolClass.SchoolClass
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&classes); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := schoolClass.UpdateClasses(a.DB, classes, "apc_database", "schoolClass"); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "success"})
 }
 
 func (a *App) deleteClasses(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	var classes []schoolClass.SchoolClass
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&classes); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := schoolClass.DeleteClasses(a.DB, classes, "apc_database", "schoolClass"); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "success"})
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -165,19 +218,77 @@ func (a *App) deleteClasses(w http.ResponseWriter, r *http.Request) {
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 func (a *App) createSubmissions(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	var submissions []submission.SubmissionCreate
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&submissions); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := submission.CreateSubmissions(a.DB, submissions, "apc_database", "submission"); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "success"})
 }
 
 func (a *App) getSubmissions(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	submissions, err := submission.GetSubmissions(a.DB, "apc_database", "submission")
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, submissions)
 }
 
 func (a *App) updateSubmissions(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	var submissions []submission.Submission
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&submissions); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := submission.UpdateSubmissions(a.DB, submissions, "apc_database", "submission"); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "success"})
 }
 
 func (a *App) deleteSubmissions(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	var submissions []submission.Submission
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&submissions); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := submission.DeleteSubmissions(a.DB, submissions, "apc_database", "submission"); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "success"})
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -185,19 +296,73 @@ func (a *App) deleteSubmissions(w http.ResponseWriter, r *http.Request) {
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 func (a *App) createTasks(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	var tasks []task.TaskCreate
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&tasks); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := task.CreateTasks(a.DB, tasks, "apc_database", "task"); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "success"})
 }
 
 func (a *App) getTasks(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	tasks, err := task.GetTasks(a.DB, "apc_database", "task")
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, tasks)
 }
 
 func (a *App) updateTasks(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	var tasks []task.Task
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&tasks); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := task.UpdateTasks(a.DB, tasks, "apc_database", "task"); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
 }
 
 func (a *App) deleteTasks(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	var tasks []task.Task
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&tasks); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := task.DeleteTasks(a.DB, tasks, "apc_database", "task"); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -205,19 +370,75 @@ func (a *App) deleteTasks(w http.ResponseWriter, r *http.Request) {
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 func (a *App) createContests(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	var contests []contest.ContestCreate
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&contests); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := contest.CreateContests(a.DB, contests, "apc_database", "contest"); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "success"})
 }
 
 func (a *App) getContests(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	contests, err := contest.GetContests(a.DB, "apc_database", "contest")
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, contests)
 }
 
 func (a *App) updateContests(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	var contests []contest.Contest
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&contests); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := contest.UpdateContests(a.DB, contests, "apc_database", "contest"); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "success"})
 }
 
 func (a *App) deleteContests(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	var contests []contest.Contest
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&contests); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := contest.DeleteContests(a.DB, contests, "apc_database", "contest"); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "success"})
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -247,13 +468,54 @@ func (a *App) createNews(w http.ResponseWriter, r *http.Request) {
 }
 
 func (a *App) getNews(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	newsArray, err := news.GetNews(a.DB, "apc_database", "news")
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, newsArray)
 }
 
 func (a *App) updateNews(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	var newsArray []news.News
+
+	body, _ := ioutil.ReadAll(r.Body)
+
+	if err := json.Unmarshal(body, &newsArray); err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := news.UpdateNews(a.DB, newsArray, "apc_database", "news"); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "success"})
 }
 
 func (a *App) deleteNews(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "function not implemented"})
+
+	var newsArray []news.News
+
+	body, _ := ioutil.ReadAll(r.Body)
+
+	if err := json.Unmarshal(body, &newsArray); err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := news.DeleteNews(a.DB, newsArray, "apc_database", "news"); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, map[string]string{"result": "success"})
 }
