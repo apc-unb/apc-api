aabea957274e443026ed3818aa533e8e303bf38d
Thiago Veras <thiago.veras.machado@gmail.com>
2019-07-26 12:00:01 -0300

    adding get taks by id and changing some attributes of tasks class

--- components/task/model.go
+++ components/task/model.go
@@ -6,13 +6,17 @@ import (
 
 type Task struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	ExamID    primitive.ObjectID `bson:"examid,omitempty"`
+	Title     string             `json:"title"`
 	Statement string             `json:"statement"`
 	Score     float32            `json:"score"`
 	Tags      []string           `json:"tags"`
 }
 
 type TaskCreate struct {
-	Statement string   `json:"statement"`
-	Score     float32  `json:"score"`
-	Tags      []string `json:"tags"`
+	ExamID    primitive.ObjectID `bson:"examid,omitempty"`
+	Title     string             `json:"title"`
+	Statement string             `json:"statement"`
+	Score     float32            `json:"score"`
+	Tags      []string           `json:"tags"`
 }
