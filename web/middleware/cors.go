303afd8e491e804a3aa8eed1b6c32b854c19f855
Thiago Veras <thiago.veras.machado@gmail.com>
2019-11-02 21:54:41 -0300

    Adding Authorization header

--- web/middleware/cors.go
+++ web/middleware/cors.go
@@ -11,7 +11,7 @@ func GetCorsMiddleware() mux.MiddlewareFunc {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE")
 			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 			next.ServeHTTP(w, r)
 		})
 	}
